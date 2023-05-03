f0653091f2e3b24f845fca97ba547976caf0c46e
Regina van der Waal <r@vdwaal.net>
2023-05-03 21:50:39 +0200

    Deelnemers urls met standaard views toegevoegd

--- WijnVoorraad/urls.py
+++ WijnVoorraad/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from . import views
+from .views import DeelnemerListView, DeelnemerDetailView
 
 app_name = 'WijnVoorraad'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:voorraad_id>/', views.detail, name='detail'),
     path('<int:voorraad_id>/drink/', views.drink, name='drink'),
+    path('deelnemers/', DeelnemerListView.as_view(), name='deelnemer_list'),
+    path('deelnemer/<pk>/', DeelnemerDetailView.as_view(), name='deelnemer_detail')
 ]
--- WijnVoorraad/views.py
+++ WijnVoorraad/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
-from .models import WijnSoort, DruivenSoort, Deelnemer, Locatie, Vak, Wijn, WijnDruivensoort, WijnVoorraad, VoorraadMutatie
+from .models import WijnSoort, DruivenSoort, Deelnemer, DeelnemerUser, Locatie, Vak, Wijn, WijnDruivensoort
+from .models import WijnVoorraad, VoorraadMutatie
 
 @login_required
 def index(request):
-    # if user.is_authenticated:
-    #     deelnemers = user.DeelnemerUser.deelnemer.objects.all()
-    #     voorraad_list = WijnVoorraad.objects.filer(Deelnemer=deelnemers(1))
-    # else:
-        # voorraad_list = WijnVoorraad.objects.all()
-
-    voorraad_list = WijnVoorraad.objects.all()
+    u = request.user
+    du = DeelnemerUser.objects.filter(user=u).values('deelnemer')
+    d = Deelnemer.objects.filter(pk__in=du)
+    voorraad_list = WijnVoorraad.objects.filter(deelnemer__in=d)  
 
     context = {'voorraad_list': voorraad_list}
     return render(request, 'WijnVoorraad/index.html', context)
@@ -25,3 +24,16 @@ def detail(request, voorraad_id):
 def drink(request, voorraad_id):
     voorraad = get_object_or_404(WijnVoorraad, pk=voorraad_id)
     return HttpResponseRedirect(reverse('WijnVooraad:detail', args=(voorraad.id,)))
+
+class DeelnemerListView(ListView):
+    model = Deelnemer
+    context_object_name = 'deelnemers'
+
+class DeelnemerDetailView(DetailView):
+    model = Deelnemer
+    context_object_name = 'deelnemer'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['voorraad_list'] = WijnVoorraad.objects.filter(deelnemer=self.object)  
+        return context

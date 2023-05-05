c09004ddbea832ff8ae1f2ed2d58e2b5e69371f9
Hendrik-Jan <h@vdwaal.net>
2023-05-05 22:06:47 +0200

    Initial setup unittesting & formating

--- WijnVoorraad/models.py
+++ WijnVoorraad/models.py
@@ -5,36 +5,40 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class WijnSoort(models.Model):
     omschrijving = models.CharField(max_length=200)
 
     def __str__(self):
         return self.omschrijving
-    
+
     class Meta:
         ordering = ["omschrijving"]
         verbose_name = "wijnsoort"
         verbose_name_plural = "wijnsoorten"
+
 
 class DruivenSoort(models.Model):
     omschrijving = models.CharField(max_length=200)
 
     def __str__(self):
         return self.omschrijving
-    
+
     class Meta:
         ordering = ["omschrijving"]
         verbose_name = "druivensoort"
         verbose_name_plural = "druivensoorten"
 
+
 class Locatie(models.Model):
     omschrijving = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.omschrijving
-    
+
     class Meta:
         ordering = ["omschrijving"]
+
 
 class Vak(models.Model):
     locatie = models.ForeignKey(Locatie, on_delete=models.CASCADE)
@@ -43,88 +47,102 @@ class Vak(models.Model):
 
     def __str__(self):
         return "%s (%s)" % (self.locatie.omschrijving, self.code)
-    
+
     class Meta:
         ordering = ["locatie", "code"]
         verbose_name_plural = "vakken"
         constraints = [
-            models.UniqueConstraint(name="unique_code_binnen_locatie",
-                                    fields=["locatie", "code"],
-                                    deferrable=Deferrable.DEFERRED,
-                                   )]
+            models.UniqueConstraint(
+                name="unique_code_binnen_locatie",
+                fields=["locatie", "code"],
+                deferrable=Deferrable.DEFERRED,
+            )
+        ]
+
 
 class Deelnemer(models.Model):
     naam = models.CharField(max_length=200)
-    standaardLocatie = models.ForeignKey(Locatie, on_delete=models.SET_NULL, blank=True,null=True)
+    standaardLocatie = models.ForeignKey(
+        Locatie, on_delete=models.SET_NULL, blank=True, null=True
+    )
     users = models.ManyToManyField(
         User,
-        through='DeelnemerUser',
-        through_fields=('deelnemer', 'user'),
+        through="DeelnemerUser",
+        through_fields=("deelnemer", "user"),
     )
 
     def __str__(self):
         return self.naam
-    
+
     class Meta:
         ordering = ["naam"]
 
 
-class DeelnemerUser (models.Model):
+class DeelnemerUser(models.Model):
     deelnemer = models.ForeignKey(Deelnemer, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s - %s" % (self.deelnemer.naam, self.user.username)
-    
+
     class Meta:
         ordering = ["deelnemer", "user"]
         verbose_name_plural = "Deelnemer users"
         constraints = [
-            models.UniqueConstraint(name="unique_deelnemer_user",
-                                    fields=["deelnemer", "user"],
-                                    deferrable=Deferrable.DEFERRED,
-                                   )]
+            models.UniqueConstraint(
+                name="unique_deelnemer_user",
+                fields=["deelnemer", "user"],
+                deferrable=Deferrable.DEFERRED,
+            )
+        ]
+
 
 class Wijn(models.Model):
     naam = models.CharField(max_length=200)
     domein = models.CharField(max_length=200)
-    wijnsoort =  models.ForeignKey(WijnSoort, on_delete=models.PROTECT)
+    wijnsoort = models.ForeignKey(WijnSoort, on_delete=models.PROTECT)
 
-    def validate_jaartal (jaartal):
+    def validate_jaartal(jaartal):
         if not (1900 < jaartal < 2500):
             raise ValidationError(
-                ('%(jaartal)s is geen geldig jaartal. Het jaartal moet liggen tussen 1901 en 2499'),
-                params={'jaartal': jaartal},
-                )
+                (
+                    "%(jaartal)s is geen geldig jaartal. Het jaartal moet liggen tussen 1901 en 2499"
+                ),
+                params={"jaartal": jaartal},
+            )
 
-    jaar = models.PositiveSmallIntegerField(null=True, blank=True, validators=[validate_jaartal])
+    jaar = models.PositiveSmallIntegerField(
+        null=True, blank=True, validators=[validate_jaartal]
+    )
     land = models.CharField(max_length=200, null=True, blank=True)
     streek = models.CharField(max_length=200, null=True, blank=True)
     classificatie = models.CharField(max_length=200, null=True, blank=True)
     leverancier = models.CharField(max_length=200, null=True, blank=True)
     website = models.URLField(max_length=200, null=True, blank=True)
     opmerking = models.CharField(max_length=200, null=True, blank=True)
-#    foto = models.ImageField(null=True, blank=True)
+    #    foto = models.ImageField(null=True, blank=True)
     datumAangemaakt = models.DateTimeField(auto_now_add=True)
     datumAfgesloten = models.DateTimeField(null=True, blank=True)
 
     wijnDruivensoorten = models.ManyToManyField(
         DruivenSoort,
-        through='WijnDruivensoort',
-        through_fields=('wijn', 'druivensoort'),
+        through="WijnDruivensoort",
+        through_fields=("wijn", "druivensoort"),
     )
 
     def __str__(self):
-        return  "%s - %s" % (self.naam, self.domein)
-    
+        return "%s - %s" % (self.naam, self.domein)
+
     class Meta:
         ordering = ["naam"]
         verbose_name_plural = "wijnen"
         constraints = [
-            models.UniqueConstraint(name="unique_wijn",
-                                    fields=["naam", "domein"],
-                                    deferrable=Deferrable.DEFERRED,
-                                   )]
+            models.UniqueConstraint(
+                name="unique_wijn",
+                fields=["naam", "domein"],
+                deferrable=Deferrable.DEFERRED,
+            )
+        ]
 
 
 class WijnDruivensoort(models.Model):
@@ -133,15 +151,18 @@ class WijnDruivensoort(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.wijn.naam, self.druivensoort.omschrijving)
-    
+
     class Meta:
         ordering = ["wijn", "druivensoort"]
         verbose_name_plural = "Wijn druivensoorten"
         constraints = [
-            models.UniqueConstraint(name="unique_wijn_druivensoort",
-                                    fields=["wijn", "druivensoort"],
-                                    deferrable=Deferrable.DEFERRED,
-                                   )]
+            models.UniqueConstraint(
+                name="unique_wijn_druivensoort",
+                fields=["wijn", "druivensoort"],
+                deferrable=Deferrable.DEFERRED,
+            )
+        ]
+
 
 class WijnVoorraad(models.Model):
     wijn = models.ForeignKey(Wijn, on_delete=models.PROTECT)
@@ -152,20 +173,30 @@ class WijnVoorraad(models.Model):
 
     def __str__(self):
         if self.vak:
-            return "%s - %s - %s (%s)" % (self.wijn.naam, self.deelnemer.naam, self.locatie.omschrijving, self.vak.code)
+            return "%s - %s - %s (%s)" % (
+                self.wijn.naam,
+                self.deelnemer.naam,
+                self.locatie.omschrijving,
+                self.vak.code,
+            )
         else:
-            return "%s - %s - %s" % (self.wijn.naam, self.deelnemer.naam, self.locatie.omschrijving)
-
+            return "%s - %s - %s" % (
+                self.wijn.naam,
+                self.deelnemer.naam,
+                self.locatie.omschrijving,
+            )
 
     class Meta:
         ordering = ["wijn", "deelnemer", "locatie", "vak"]
         verbose_name = "Wijnvoorraad"
         verbose_name_plural = "wijnvoorraad"
         constraints = [
-            models.UniqueConstraint(name="unique_wijnvoorraad",
-                                    fields=["wijn", "deelnemer", "locatie", "vak"],
-                                    deferrable=Deferrable.DEFERRED,
-                                   )]
+            models.UniqueConstraint(
+                name="unique_wijnvoorraad",
+                fields=["wijn", "deelnemer", "locatie", "vak"],
+                deferrable=Deferrable.DEFERRED,
+            )
+        ]
 
 
 class VoorraadMutatie(models.Model):
@@ -174,18 +205,23 @@ class VoorraadMutatie(models.Model):
     locatie = models.ForeignKey(Locatie, on_delete=models.PROTECT)
     vak = models.ForeignKey(Vak, on_delete=models.PROTECT, null=True, blank=True)
 
-    IN = 'I'
-    UIT = 'U'
-    in_uit_choices = [(IN, 'In'),
-                      (UIT, 'Uit'),
-                      ]
+    IN = "I"
+    UIT = "U"
+    in_uit_choices = [
+        (IN, "In"),
+        (UIT, "Uit"),
+    ]
     in_uit = models.CharField(max_length=1, choices=in_uit_choices)
     datum = models.DateField()
     aantal = models.IntegerField()
     omschrijving = models.CharField(max_length=200)
 
     def __str__(self):
-         return "%s - %s - %s" % (self.wijn.naam, self.deelnemer.naam, self.datum.strftime("%d-%m-%Y"))
+        return "%s - %s - %s" % (
+            self.wijn.naam,
+            self.deelnemer.naam,
+            self.datum.strftime("%d-%m-%Y"),
+        )
 
     class Meta:
         verbose_name = "voorraadmutatie"
